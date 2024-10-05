844d0f43cb3aa338070c5aef09b13d2d87186b1c
Malek Mansour <malek24mansour@gmail.com>
2024-10-05 12:49:25 -0600

    Update mainpage.js

--- Minesweeper/components/mainpage.js
+++ Minesweeper/components/mainpage.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const MainPage = () => {
-    return (
-        <div>
-            <h1>Welcome to the Main Page</h1>
-            <p>This is a simple React component.</p>
-        </div>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>This is the Main Page!</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff330',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default MainPage;

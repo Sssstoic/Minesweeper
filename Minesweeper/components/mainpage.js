7c415610bbe35e5b3e351edc41aa38e54c36cd7c
Malek Mansour <malek24mansour@gmail.com>
2024-10-06 00:41:23 -0600

    Update mainpage.js

--- Minesweeper/components/mainpage.js
+++ Minesweeper/components/mainpage.js
@@ -35,15 +35,6 @@ const MainPage = () => {
         style={styles.logo}
       />
 
-      {/* Instructions */}
-      <Text style={styles.instructions}>
-        Welcome to Minesweeper! The goal is to clear all the cells without detonating any mines.
-        - Tap on a cell to reveal what's underneath.
-        - Numbers show how many mines are adjacent to that cell.
-        - Long press to flag a cell if you think there's a mine.
-        Clear the board to win. Good luck!
-      </Text>
-
       {/* Difficulty Slider */}
       <Text style={[styles.difficultyText, { color: getDifficultyColor(difficulty) }]}>
         {getDifficultyText(difficulty)}

72da011bb06faebe6b4e9b9f3e594ee921e76720
Malek Mansour <malek24mansour@gmail.com>
2024-10-06 22:33:14 -0600

    Update app.js

--- Minesweeper/app.js
+++ Minesweeper/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import MainPage from './components/MainPage'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainPage from './screens/MainPage'; // Ensure this component exists
+import Minesweeper from './screens/Minesweeper';
 
-function App() {
-    return (
-        <div className="App">
-            <MainPage /> 
-        </div>
-    );
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainPage">
+        <Stack.Screen name="MainPage" component={MainPage} />
+        <Stack.Screen name="Minesweeper" component={Minesweeper} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default App;
